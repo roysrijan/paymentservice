845a949c269da263217d4e45472a266351c1a0d6
roysrijan <roy.srijan@outlook.com>
2021-06-16 21:53:02 +0530

    Create Subscription Plans and Suscriptions

--- src/services/subscription.service.js
+++ src/services/subscription.service.js
@@ -1,15 +1,34 @@
+const axios = require("axios");
 
 
 //======= Create Subscription =======
 exports.createSubscription = async (body) => {
     const notify = body;
     // Updates Live Notification
-    return notify;
+    const rawResponse = await axios.post( 'https://test.cashfree.com/api/v2/subscriptions',
+    body,
+    {  headers: {
+        'X-Client-Id': process.env.appId,
+        'X-Client-Secret': process.env.secretKey,
+        'Content-Type': 'application/json'
+        }
+    }
+  )
+    return rawResponse.data;
 }
 
 //======= Create Subscription Plans=======
 exports.createSubscriptionPlans = async (body) => {
     const notify = body;
     // Updates Live Notification
-    return notify;
+    const rawResponse = await axios.post( 'https://test.cashfree.com/api/v2/subscription-plans',
+    body,
+    {  headers: {
+        'X-Client-Id': process.env.appId,
+        'X-Client-Secret': process.env.secretKey,
+        'Content-Type': 'application/json'
+        }
+    }
+  )
+    return rawResponse.data;
 }
